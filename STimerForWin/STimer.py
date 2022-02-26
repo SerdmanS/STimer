bcec42c285a0219ebd1731029a4474b00ac0a413
L4Z4R3 <51765009+SerdmanS@users.noreply.github.com>
2022-02-26 14:21:14 +0300

    Update STimer.py

--- STimerForWin/STimer.py
+++ STimerForWin/STimer.py
@@ -3,14 +3,14 @@
 # Aslında C# form uygulaması yapacaktım ama VS19 kullanırken yaptığım şeyi VS22 ile yapamayınca sinirlenip daha az satırla python kullanarak yapmak istedim
 
 import time, os, sys
-
-cmd = ''
+cmd = ""
+flag = False
 def help():
     print("\n[*] Program isminden sonra çalışmasını istediğiniz komutu girin\n\n\tÖrnek: python STimer.py tasklist\n\t-s veya --shutdown : Sistemi kapatır\n\t-r veya --restart : Sistemi yeniden başlatır\n")
     sys.exit(1)
     return
-
 def clock():
+    comd = cmd
     H = int(input("Saat girin : "))
     M = int(input("Dakika girin : "))
     S = int(input("Saniye girin : "))
@@ -24,11 +24,14 @@ def clock():
         if H == 0 and M == 0 and S == 0:
             print("\n[*] Süre doldu!\n")
             time.sleep(1)
+            if flag == True:
+                print(f"[*] Komut işlendi : {comd}")
+                os.system(comd)
             if len(sys.argv)>1:
                 for i in range(1,len(sys.argv)):
-                    cmd = cmd+sys.argv[i]+" "
-                print(f"[*] Komut işlendi : {cmd}")
-                os.system(cmd)
+                    comd = comd+sys.argv[i]+" "
+                print(f"[*] Komut işlendi : {comd}")
+                os.system(comd)
             break
 
         if S==0 and M>0:
@@ -45,10 +48,12 @@ try:
     if len(sys.argv)>1:
         if sys.argv[1] == '-h' or sys.argv[1] == '--help': help()
         if sys.argv[1] == '-s' or sys.argv[1] == '--shutdown':
-            cmd = "shutdown -p 0"
+            flag = True
+            cmd = "shutdown -p -t 0"
             clock()
         if sys.argv[1] == '-r' or sys.argv[1] == '--restart':
-            cmd = "shutdown -r 0"
+            flag = True
+            cmd = "shutdown -r -t 0"
             clock()
         else :
             clock()
